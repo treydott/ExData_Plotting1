7b78e286046ccfaf25e4907b78bb0398a8834caf
treydott <jeubuckdott@gmail.com>
2016-02-15 18:06:24 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -2,14 +2,21 @@
 table1 <- read.csv("household_power_consumption.txt", colClasses = "character", sep=";")
 table1 <- table1[table1[,1]=="1/2/2007" | table1[,1]=="2/2/2007",]
 
-## Converts to numeric
+## Converts numbers to numeric
 for (i in 3:ncol(table1)) {
   table1[,i] <- suppressWarnings(as.numeric(table1[,i]))
 }
 
-## Plots histogram
-hist(table1$Global_active_power, col="red", main="Global Active Power", xlab = "Global Active Power (kilowatts)")
+## Converts date & time to POSIXct
+table1[,1] <- as.Date(table1[,1], format = "%d/%m/%Y")
+date_time <- as.POSIXct(paste(table1$Date,table1$Time))
+
+## Plots submetering data
+with(table1, plot(x=date_time, y=Sub_metering_1, type='l', xlab="", ylab = "Energy sub metering"))
+with(table1, points(date_time, Sub_metering_2, type='l', col="Red"))
+with(table1, points(date_time, Sub_metering_3, type='l', col="Blue"))
+legend("topright", legend=(c('Sub_metering_1','Sub_metering_2','Sub_metering_3')), lty=1, col = c("black","red","blue"))
 
 ## Saves as png file
-dev.copy(png, file="plot1.png", width=580, height=538)
+dev.copy(png, file="plot3.png", width=580, height=538)
 dev.off()
